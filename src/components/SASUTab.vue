9056798b60963a6ad3618a0067389a95ea68e165
hlassiege <hugo@malt.com>
2022-01-09 15:46:56 +0100

    Mise en page

--- src/components/SASUTab.vue
+++ src/components/SASUTab.vue
@@ -1,88 +1,219 @@
 <template>
   <div class="container">
-    <div class="card">
-      <div class="card-header">
-        Paramètres
-      </div>
-      <div class="card-body">
-        <h5 class="card-title">Veuillez renseigner les paramètres nécessaires pour calculer vos revenus</h5>
-        <p class="card-text">Plus de paramètres sont à votre disposition plus bas</p>
-        <form>
-          <div class="row">
-            <div class="col-md-6">
-              <label for="tjm" class="form-label">TJM (votre tarif journalier)</label>
-              <input type="text" v-model="tjm" class="form-control" id="tjm" placeholder="600">
-            </div>
-            <div class="col-md-6">
-              <label for="workload" class="form-label">Nombre de jours travaillés dans l'année</label>
-              <input type="text" v-model="workload" class="form-control" id="workload" placeholder="200">
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-    <div class="card">
-      <div class="card-header">
-        Paramètres avancés
-      </div>
-      <div class="card-body">
-        <form>
-          <div class="row">
-            <div class="col-md-6">
-              <label for="expenses" class="form-label">Frais annuel</label>
-              <div class="input-group">
-                <span class="input-group-text" id="euros">€</span>
-              <input type="text" v-model="expenses" class="form-control" id="expenses" placeholder="10000" aria-describedby="euros">
+    <div class="row">
+      <div class="card">
+        <div class="card-header">
+          Paramètres
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Veuillez renseigner les paramètres nécessaires pour calculer vos revenus</h5>
+          <p class="card-text">Plus de paramètres sont à votre disposition plus bas</p>
+          <form>
+            <div class="row">
+              <div class="col-md-6">
+                <label for="tjm" class="form-label">TJM (votre tarif journalier)</label>
+                <input type="text" v-model="tjm" class="form-control" id="tjm" placeholder="600">
+              </div>
+              <div class="col-md-6">
+                <label for="workload" class="form-label">Nombre de jours travaillés dans l'année</label>
+                <input type="text" v-model="workload" class="form-control" id="workload" placeholder="200">
               </div>
             </div>
-             <div class="col-md-6">
-              <label for="shareOfSalary" class="form-label">Pourcentage versé en salaire (versus dividendes)</label>
-              <div class="input-group">
-               <span class="input-group-text" id="percent">%</span>
-              <input type="text" v-model="shareOfSalary" class="form-control" id="shareOfSalary" placeholder="15" aria-describedby="percent">
-              </div>
-            </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
-    <div class="row">
-      <ul>
-        <li>Benefice :  {{ benefits }}</li>
-        <li>Salaire versé BRUT :  {{ grossSalary }}</li>
-        <li>Cotisations sociales :  {{ socialContributions }}</li>
-        <li>Salaire versé NET :  {{ netSalary }}</li>
-        <li>Impot société : {{ impotSociete }} </li>
-        <li>Dividende brut : {{ dividends }}</li>
-        <li>CSG/RDF sur dividendes : {{ csgRdsOnDividends }} </li>
-        <li>Dividendes perçus : {{ dividendsReceivedAfterCSGRDS }} </li>
-        <li>Dividendes imposable (abattement 40%) : {{ taxableDividends }} </li>
-        <li>Total reçu : {{ totalRevenue }}</li>
-        <li>Total percu imposable : {{ totalTaxableRevenue }}</li>
-        <li>Impot sur le revenu : {{ impotRevenu }}</li>
-        <li>Net après Impot (in my pocket) : {{ totalRevenue -  impotRevenu }}</li>
-      </ul>
 
-    </div>
-
+    
+    
     <div class="row">
-      <div class="card col-md-3">
+      <div class="card text-center col-md-3">
           <div class="card-body">
-            <h6 class="card-title">Chiffre d'affaire annuel: </h6>
+            <h6 class="card-title text-uppercase text-muted">Chiffre d'affaire annuel: </h6>
             <p class="card-text red-text">€<span class="ml-2" style="font-size: 30px;">{{ revenue }}</span></p>
           </div>
       </div>
-      <div class="card col-md-3">
+      <div class="card text-center col-md-3">
           <div class="card-body">
-            <h6 class="card-title">In my pocket (après IR): </h6>
+            <h6 class="card-title text-uppercase text-muted">In my pocket (après IR): </h6>
             <p class="card-text red-text">€<span class="ml-2" style="font-size: 30px;">{{ totalRevenue -  impotRevenu }}</span></p>
           </div>
       </div>
     </div>
 
 
-    
+    <div class="row">
+      <div class="card">
+        
+        <div class="card-body">
+          <div class="accordion accordion-flush" id="accordionAdvancedSettings">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="flush-headingAdvancedSettings">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-advancedSettings" aria-expanded="false" aria-controls="flush-advancedSettings">
+                  Paramètres avancés
+                </button>
+              </h2>
+              <div id="flush-advancedSettings" class="accordion-collapse collapse" aria-labelledby="flush-headingAdvancedSettings" data-bs-parent="#accordionAdvancedSettings">
+                <div class="accordion-body">
 
+                  <form>
+                    <div class="row">
+                      <div class="col-md-6">
+                        <label for="expenses" class="form-label">Frais annuel</label>
+                        <div class="input-group">
+                          <span class="input-group-text" id="euros">€</span>
+                        <input type="text" v-model="expenses" class="form-control" id="expenses" placeholder="10000" aria-describedby="euros">
+                        </div>
+                      </div>
+                      <div class="col-md-6">
+                        <label for="shareOfSalary" class="form-label">Pourcentage versé en salaire (versus dividendes)</label>
+                        <div class="input-group">
+                        <span class="input-group-text" id="percent">%</span>
+                        <input type="text" v-model="shareOfSalary" class="form-control" id="shareOfSalary" placeholder="15" aria-describedby="percent">
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="card">
+        
+        <div class="card-body">
+          <div class="accordion accordion-flush" id="accordionDetails">
+          
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="flush-headingOne">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                Details
+              </button>
+            </h2>
+            <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionDetails">
+              <div class="accordion-body">
+                <div class="row">
+                  <div class="col">
+                    <h4>Dépenses</h4>
+                    <table class="table table-borderless">
+                      <tbody>
+                        <tr>
+                          <th scope="row">Frais</th>
+                          <td>{{ expenses }}€</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">Salaire super brut</th>
+                          <td>{{ grossSalary }}€</td>
+                        </tr>
+                        <tr style="border-top: 1px solid #ff000d;">
+                          <th scope="row">Total charges sociétés</th>
+                          <td>{{ grossSalary + expenses }}€</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                  <div class="col">
+                    <h4>Bénéfices</h4>
+                    <table class="table table-borderless">
+                      <tbody>
+                        <tr>
+                          <th scope="row">Chiffre d'affaires</th>
+                          <td>{{ revenue }}€</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">Charges sociétés</th>
+                          <td>-{{ grossSalary + expenses }}€</td>
+                        </tr>
+                        <tr class="lineTotal">
+                          <th scope="row" >Bénéfice brut</th>
+                          <td>{{ benefits }}€</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">Impot société</th>
+                          <td>-{{ impotSociete  }}€</td>
+                        </tr>
+                        <tr class="lineTotal">
+                          <th scope="row">Bénéfice net <small>(dividendes versables)</small></th>
+                          <td>{{ dividends }}€</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                  <div class="col">
+                    <h4>Dividendes</h4>
+                    <table class="table table-borderless">
+                      <tbody>
+                        <tr>
+                          <th scope="row">Dividendes brut</th>
+                          <td>{{ dividends  }}€</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">CSG/RDS sur dividendes</th>
+                          <td>-{{ csgRdsOnDividends  }}€</td>
+                        </tr>
+                        <tr class="lineTotal">
+                          <th scope="row">Dividendes perçus</th>
+                          <td>{{ dividendsReceivedAfterCSGRDS  }}€</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>    
+
+                  <div class="col">
+                    <h4>Revenus personnels</h4>
+                    <table class="table table-borderless">
+                      <tbody>
+                        <tr>
+                          <th scope="row">Salaire super brut</th>
+                          <td>{{ grossSalary }}€</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">Cotisations sociales <small> (patronales + salariales)</small></th>
+                          <td>-{{ socialContributions }}€</td>
+                        </tr>
+                        <tr class="lineTotal">
+                          <th scope="row" >Salaire versé net</th>
+                          <td>{{ netSalary }}€</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">Dividendes perçus</th>
+                          <td>{{ dividendsReceivedAfterCSGRDS  }}€</td>
+                        </tr>
+                        <tr>
+                          <td >Dividendes imposables <small>(abattement 40%)</small></td>
+                          <td>{{ taxableDividends  }}€</td>
+                        </tr>
+                        <tr class="lineTotal">
+                          <th scope="row">Total Reçu</th>
+                          <td>{{ totalRevenue  }}€</td>
+                        </tr>
+                      <tr>
+                          <th scope="row">Total imposable</th>
+                          <td>{{ totalTaxableRevenue  }}€</td>
+                        </tr>                      
+                        <tr>
+                          <th scope="row">Impot sur le revenu</th>
+                          <td>-{{ impotRevenu  }}€</td>
+                        </tr>
+                        <tr class="lineTotal">
+                          <th scope="row">Net après impot</th>
+                          <td><strong>{{ totalRevenue -  impotRevenu }}€</strong></td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>                
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -189,5 +320,8 @@ export default {
 .card {
   margin-top: 20px;
   margin-right: 10px;
+}
+.lineTotal {
+  border-top: 1px solid #ff000d;
 }
 </style>
